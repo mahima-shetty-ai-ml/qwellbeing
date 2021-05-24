2b82aafdd0a617eef0ae3c05e66385a1e4ec7482
neha1304 <neha1304.v@gmail.com>
2021-05-24 21:26:11 +0530

    report style to landscape

--- pdf.js
+++ pdf.js
@@ -10,7 +10,7 @@ window.onload = function () {
                 image: { type: 'jpeg', quality: 0.98 },
                 html2canvas: { scale: 2 },
                 jsPDF: { unit: 'in', format: 'A3', orientation: 'landscape' },
-                pagebreak:{after:['#card1','#card2','#card3','#card4']}
+                pagebreak:{after:['#card1','#card3']}
             };
             html2pdf().from(invoice).set(opt).save();
         })
